bbc258e23a355c8b8f2ee4b08d2b653c6011d120
Utilisateur invité <Guest@iMac-de-admin-106.local>
2023-06-08 15:19:17 +0200

    [feat] :sparkles: complete the assignement

--- swift-meuuuh/ContentView.swift
+++ swift-meuuuh/ContentView.swift
@@ -39,26 +39,87 @@ extension View {
 }
 
 
+class AudioPlayer: Equatable {
+    let player: AVAudioPlayer
+
+    init(player: AVAudioPlayer) {
+        self.player = player
+    }
+
+    static func == (lhs: AudioPlayer, rhs: AudioPlayer) -> Bool {
+        return lhs.player === rhs.player
+    }
+}
+
+//
 
 
 struct ContentView: View {
-    @State private var boiteMeuhLecteur: AVAudioPlayer?
-    
+    let sonsArray = [
+        "AMBBird_Reveil des oiseaux 3 (ID 0999)_LS": "Réveil des oiseaux",
+        "ANMLCat_Miaulement chat 2 (ID 1890)_LS": "Miaulement chat",
+        "ANMLDog_Chien qui aboie 3 (ID 2955)_LS": "Chien qui aboie",
+        "ANMLFarm_Canards (ID 0276)_LS": "Canards",
+        "ANMLFarm_Poules et ponte (ID 0978)_LS": "Poules et ponte",
+        "ANMLFarm_Vache qui meugle (ID 0546)_LS": "Vache qui meugle",
+        "ANMLHors_Hennissement de cheval 3 (ID 0863)_LS": "Cheval",
+        "ANMLInsc_Grillon champetre male (ID 1020)_LS": "Grillon champetre",
+        "BIRDCrow_Corneilles 2 (ID 0956)_LS": "Corneilles",
+        "BIRDPrey_Chouette hulotte 2 (ID 1764)_LS": "Chouette hulotte",
+        "BIRDPrey_Hibou moyen duc (ID 0936)_LS": "Hibou moyen duc",
+        "BIRDSong_Rouge gorge 4 (ID 1670)_LS": "Rouge gorge",
+        "FOLYFeet_Galop de cheval sur sol dur (ID 0611)_LS": "Galop cheval sur sol dur",
+    ]
+
+    @State private var selectedSound = "BIRDPrey_Hibou moyen duc (ID 0936)_LS"
+    @State private var currentPlayingSound: String?
+
+    @State private var soundPlayers: [String: AudioPlayer]?
+
     var body: some View {
-        Text("Secouez le téléphone !").onShake {
-            playSound()
+        VStack {
+            List(sonsArray.keys.sorted(), id: \.self) { sound in
+                HStack {
+                    Text(self.sonsArray[sound] ?? sound)
+                    Spacer()
+                    if sound == selectedSound {
+                        Image(systemName: "checkmark")
+                    }
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    self.selectedSound = sound
+                }
+            }
+
+            Text("Secouez-moi!")
+                .onShake {
+                    // Stop the current playing sound if there is one
+                    if let currentPlayingSound = currentPlayingSound {
+                        self.soundPlayers?[currentPlayingSound]?.player.stop()
+                    }
+
+                    // Play the selected sound
+                    self.soundPlayers?[self.selectedSound]?.player.play()
+
+                    // Update the current playing sound
+                    self.currentPlayingSound = self.selectedSound
+                }
         }
+        .onAppear(perform: loadSounds)
     }
-    
-    func playSound() {
-        let path = Bundle.main.path(forResource: "media/AMBBird_Reveil des oiseaux 3 (ID 0999)_LS" , ofType: "wav")!
-        let url  = URL(fileURLWithPath: path)
-        
-        do {
-            boiteMeuhLecteur = try AVAudioPlayer(contentsOf: url)
-            boiteMeuhLecteur?.play()
-        } catch {
-            print("Pas possible de lire le son")
+
+    private func loadSounds() {
+        var players: [String: AudioPlayer] = [:]
+
+        for sound in sonsArray.keys {
+            let path = Bundle.main.path(forResource: "media/" + sound, ofType: "wav")!
+            let url = URL(fileURLWithPath: path)
+            let player = try! AVAudioPlayer(contentsOf: url)
+
+            players[sound] = AudioPlayer(player: player)
         }
+
+        soundPlayers = players
     }
 }
